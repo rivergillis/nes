e3b509faae121ee19ef8bb249281f4624580df3a
River Gillis <jrivergillis@gmail.com>
2021-07-21 20:13:03 -0700

    mem info

--- cpu6502.h
+++ cpu6502.h
@@ -6,4 +6,24 @@ class Cpu6502 {
 
 };
 
+// notes: 8-bit addressing
+// NES lacks decimal mode.
+
+// mem notes:
+// $0000-$07FF SIZE $0800 : 2KB internal RAM 
+// $0800-$0FFF SIZE $0800 : mirror of $0000 - $07FF
+// $1000-$17FF SIZE $0800 : mirror of $0000 - $07FF
+// $1800-$1FFF SIZE $0800 : mirror of $0000 - $07FF
+// $2000-$2007 SIZE $0008 : NES PPU registers
+// $2008-$3FFF SIZE $1FF8 : mirrors of $2000 - $2007 (repeats every 8 bytes)
+// $4000-$4017 SIZE $0018 : NES APU and I/O registers
+// $4018-$401F SIZE $0008 : APU and I/o functionality that is normally disabled (CPU test mode)
+// $4020-$FFFF SIZE $BFE0 : Cartidge space: PRG ROM, PRG RAM, and mapper registers
+
+// Think of 256-byte "page". So internal RAM $0000-$07FF has 8 pages. 
+// The optional PRG RAM chip on some carts is 32 pages of SRAM at $6000-$7FFF
+// Indirect addressing relies on zero/direct page at $0000-$00FF.
+// Stack instructions always access stack page at $0100-$01FF.
+
+
 #endif
