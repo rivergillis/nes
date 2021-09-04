958e56a7214ca7a2375999e79680369d65b4be9d
River Gillis <jrivergillis@gmail.com>
2021-09-04 16:59:00 -0700

    test pass colors

--- test.sh
+++ test.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
 make clean
 make nes TEST_DEFINES="-U DEBUG -D NESTEST"
 ./nes 2>&1 | tee test/out.log
 
 ## Ignore PPU:
-# sed 's/PPU:.*C/CYC/g' test/out.log > test/out-noppu.log
-# diff --brief test/out-noppu.log test/nestest_golden-noppu.log
+sed 's/PPU:.*C/CYC/g' test/out.log > test/out-noppu.log
+diff --brief test/out-noppu.log test/nestest_golden-noppu.log
 
-# if [ $? ]; then
-#   code --diff test/out-noppu.log test/nestest_golden-noppu.log
-# fi
+if [[ $? -eq 1 ]]; then
+  code --diff test/out-noppu.log test/nestest_golden-noppu.log
+else
+  echo -e "${GREEN}PASSED${NC} -- nes2x output matches nestest.nes golden (cpu-only)"
+fi
 
 ## Don't ignore PPU:
-diff --brief test/out.log test/nestest_golden.log
+# diff --brief test/out.log test/nestest_golden.log
 
-if [ $? ]; then
-  code --diff test/out.log test/nestest_golden.log
-fi
+# if [ $? ]; then
+#   code --diff test/out.log test/nestest_golden.log
+# fi
