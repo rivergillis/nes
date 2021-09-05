6b82a006d829bc667e46d6c1db451402e942c369
River Gillis <jrivergillis@gmail.com>
2021-09-04 17:29:46 -0700

    cycles arg

--- main.cpp
+++ main.cpp
@@ -8,28 +8,36 @@
 // good log: https://www.qmtpro.com/~nes/misc/nestest.log
 // start execution at $C000
 const std::string kTestRomPath = "/Users/river/code/nes/roms/nestest.nes";
+const uint64_t kDefaultNumCycles = 8991; // nestest
 
-void Run(const std::string& rom_path) {
+void Run(const std::string& rom_path, uint64_t num_cycles) {
   Cpu6502 cpu(rom_path);
       #ifdef DEBUG
       auto start_time = Clock::now();
       #endif
-  for (int i = 0; i < 8991; i++) {
+  for (int i = 0; i < num_cycles; i++) {
     cpu.RunCycle();
   }
-  DBG( "Executed 8991 instructions in %s\n", StringMsSince(start_time).c_str());
+  DBG( "Executed %llu instructions in %s\n", num_cycles, StringMsSince(start_time).c_str());
 }
 
 std::string GetFileName(int argc, char* argv[]) {
-  if (argc != 2) {
-    throw std::runtime_error("Usage: nes2x path/to/file");
+  if (argc < 2) {
+    throw std::runtime_error("Usage: nes2x path/to/file [optional: num_cycles]");
   }
   return std::string(argv[1]);
 }
 
+uint64_t GetNumCycles(int argc, char* argv[]) {
+  if (argc < 3) {
+    return kDefaultNumCycles;
+  }
+  return std::stoull(argv[2]);
+}
+
 int main(int argc, char* argv[]) {
   try {
-    Run(GetFileName(argc, argv));
+    Run(GetFileName(argc, argv), GetNumCycles(argc, argv));
     DBG("Exit main() success\n");
   } catch (const std::exception& e) {
     std::cerr << "ERROR: " << e.what() << std::endl;
